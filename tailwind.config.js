f8bde70b24688a381e8312b0c7e202ec151202d4
mirkvmol <ataxanovmirkamol@gmail.com>
2025-04-24 21:00:32 +0500

    installed local tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
-tailwind.config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
         main: "#fdc900",
-        gray: "#767676",
+        graylight: "#838383",
         primary: "#69b99d",
         secondary: "#0f241d",
+        orangedark: "#23250e",
+        blue: "#2a63ff",
+        bluedark: "#1e3a8a",
+      },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+      animation: {
+        scroll: "scroll 30s linear infinite",
       },
     },
   },
+  plugins: [],
 };
